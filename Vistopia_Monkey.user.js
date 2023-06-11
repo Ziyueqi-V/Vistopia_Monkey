9356eac51b504dda8ab4a5c97fd6d7a994a16e49
Ziyueqi-V <1208896581@qq.com>
2023-06-11 09:05:27 +0800

    release v0.0.6

--- Vistopia_Monkey.user.js
+++ Vistopia_Monkey.user.js
@@ -1,16 +1,20 @@
 // ==UserScript==
-// @name         Vistopia_Monkey - 理想猴
-// @namespace    http://tampermonkey.net/
-// @version      0.0.3
-// @description  一款优化Vistopia体验的油猴脚本
-// @author       Sherlock-V
-// @match        https://www.vistopia.com.cn
-// @match        https://www.vistopia.com.cn/*
-// @grant        GM_addStyle
-// @run-at       document-body
-// @license      MIT
-// @homepageURL  https://github.com/Ziyueqi-V/Vistopia_Monkey
-// @supportURL   https://github.com/Ziyueqi-V/Vistopia_Monkey
+// @name            Vistopia_Monkey
+// @name:cn         理想猴
+// @version      0.0.6
+// @namespace       http://tampermonkey.net/
+// @description     This userscript does wonderful things
+// @description:cn  看理想网页界面自定义
+// @description     一款优化Vistopia体验的油猴脚本
+// @author          Sherlock-V
+// @match           https://www.vistopia.com.cn
+// @match           https://www.vistopia.com.cn/*
+// @grant           GM_addStyle
+// @run-at          document-body
+// @license         MIT
+// @homepageURL     https://github.com/Ziyueqi-V/Vistopia_Monkey
+// @supportURL      https://github.com/Ziyueqi-V/Vistopia_Monkey
+// @updateURL       https://github.com/Ziyueqi-V/Vistopia_Monkey/blob/main/Vistopia_Monkey.user.js
 // ==/UserScript==
 (function () {
   'use strict';
